6d1f8e8618837138388d75dcf20b06c51e1bc239
Pedro Schuster <pedro.ssc42@gmail.com>
2023-04-11 20:44:12 -0300

    Resolucao exercicios DOM e JS2

--- praticaJS2/js/script.js
+++ praticaJS2/js/script.js
@@ -1,8 +1,110 @@
+var textoEmBrancoValido = false;
+var emailValido = false;
+var cpfValido = false;
+var senhaValida = false;
+
+
+function NotImplementedError(message = "") {
+    this.name = "NotImplementedError";
+    this.message = message;
+  }
+
+  NotImplementedError.prototype = new Error();;
+  
+
+class Cpf{
+    constructor (valor){
+        this._valor = valor;
+    }
+
+    #haOnzeDigitos = function(cpf) {
+        if ( cpf.split("").length == 11){
+            return true;
+        }
+        throw new NotImplementedError("Erro! CPF tem que ter 11 numeros");
+
+    }
+    
+    #todosOsOnzeDigitosSaoNumeros(cpf) {
+        //---- edite aqui para a validação do exercício 9b
+        if (!isNaN(cpf)){
+            return true;
+        }
+        throw new NotImplementedError("Erro! CPF possui valores não numericos");
+
+    }
+    
+    #osOnzeNumerosSaoDiferentes(cpf) {
+        //---- edite aqui para a validação do exercício 9c
+        for (let index = 0; index < cpf.length - 1; index++) {
+            if (cpf[index] != cpf[index + 1]){
+                break
+            }
+    
+            if(index + 2 == cpf.length){
+                throw new NotImplementedError("Erro! CPF possui numeros iguais!");
+            }
+        }
+    
+        return true
+    }
+    
+    #oPrimeiroDigitoVerificadorEhValido(cpf) {
+        //---- edite aqui para a validação do exercício 9d
+        let soma = 0;
+        for (let index = 0; index < cpf.length - 2; index++) {
+            soma += cpf[index] * (cpf.length - 1 - index);
+        }
+        soma *= 10;
+        soma %= 11;
+    
+        if (soma == 10){
+            soma = 0;
+        }
+        if (soma == cpf[9]){
+            return true;
+        }
+        throw new NotImplementedError("Erro no primeiro digito verificador");
+
+    }
+    
+    #oSegundoDigitoVerificadorEhValido(cpf) {
+        //---- edite aqui para a validação do exercício 9e
+        let soma = 0;
+        for (let index = 0; index < cpf.length - 1; index++) {
+            soma += cpf[index] * (cpf.length - index);
+        }
+        soma *= 10;
+        soma %= 11;
+    
+        if (soma == 10){
+            soma = 0;
+        }
+        if (soma == cpf[10]){
+            return true;
+        }
+        throw new NotImplementedError("Erro no segundo digito verificador");
+    }
+
+    validateCpf(){
+        try{
+            this.#haOnzeDigitos(this._valor);
+            this.#todosOsOnzeDigitosSaoNumeros(this._valor);
+            this.#osOnzeNumerosSaoDiferentes(this._valor);
+            this.#oPrimeiroDigitoVerificadorEhValido(this._valor);
+            this.#oSegundoDigitoVerificadorEhValido(this._valor);
+            return true;
+        }
+        catch (exception){
+            return exception;
+        }
+    }
+}
+
 function mostrarApenasHome(){
     document.getElementById("login-body").style.display = "none";
     document.getElementById("nova-conta").style.display = "none";
     document.getElementById("divHome").style.display = "block";
-
 }
 
 function mostrarApenasLogin(){
@@ -26,11 +128,102 @@ function updateInput(){
 
 
 function mostrarApenasConta(){
+    document.getElementById("nova-conta").children[1].reset();
+    document.getElementById("areaBotaoConta").children[0].disabled = true;
+
     document.getElementById("login-body").style.display = "none";
     document.getElementById("divHome").style.display = "none";
     document.getElementById("nova-conta").style.display = "block";
 }
 
-function validaTextoEmBranco(){
+function validaTextoEmBranco(obj, id){
+    let msg = document.getElementById(id);
 
+    if (!obj.value){
+        msg.style.display = "block";
+        msg.style.color = "red";
+        msg.textContent = "Campo obrigatório!";
+        textoEmBrancoValido = false;
+    }
+    else{
+        msg.style.display = "block";
+        msg.style.color = "green";
+        msg.textContent = "Válido!";
+        textoEmBrancoValido = true;
+    }
+    validarForm()
+}
+
+function validaEmail(obj, id){
+    let msg = document.getElementById(id);
+    if (!obj.value){
+        msg.style.display = "block";
+        msg.style.color = "red";
+        msg.textContent = "Campo obrigatório!"
+        emailValido = false;
+    }
+    else if (obj.value.split("@").length - 1 != 1){
+        msg.style.display = "block";
+        msg.style.color = "red";
+        msg.textContent = "Campo invalido!"
+        emailValido = false;
+    }
+    else{
+        msg.style.display = "block";
+        msg.style.color = "green";
+        msg.textContent = "Válido!";
+        emailValido = true;
+    }
+    validarForm()
+}
+
+function validaSenha(obj, id){
+    let msg = document.getElementById(id);
+    if (!obj.value){
+        msg.style.display = "block";
+        msg.style.color = "red";
+        msg.textContent = "Campo obrigatório!"
+        senhaValida = false;
+    }
+    else if (obj.value !== document.getElementById("senha").value){
+        msg.style.display = "block";
+        msg.style.color = "red";
+        msg.textContent = "Senhas estão diferentes!"
+        senhaValida = false;
+    }
+    else{
+        msg.style.display = "block";
+        msg.style.color = "green";
+        msg.textContent = "Válido!";
+        senhaValida = true;
+    }
+    validarForm()
+}
+
+function validarCPF(obj, id){
+    let msg = document.getElementById(id);
+    var cpf = new Cpf(obj.value);
+    var validacao = cpf.validateCpf();
+    if (validacao === true){
+        msg.style.display = "block";
+        msg.style.color = "green";
+        msg.textContent = "Válido!";
+        cpfValido = true;
+    }
+    else{
+        msg.style.display = "block";
+        msg.style.color = "red";
+        msg.textContent = validacao.message;
+        cpfValido = false;
+    }
+    validarForm()
+}
+
+function validarForm(){
+    if (textoEmBrancoValido && emailValido && senhaValida && cpfValido){
+        document.getElementById("areaBotaoConta").children[0].disabled = false;
+    }
+    else{
+        document.getElementById("areaBotaoConta").children[0].disabled = true;
+    }
 }
